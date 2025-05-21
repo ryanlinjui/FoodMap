99030dabae07aa74a335d1b0ffc0869cc6c104c7
ryanlinjui <ryanlinjui@gmail.com>
2025-05-21 17:25:56 +0800

    chore: init

---
+++ FoodMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodMap
+//
+//  Created by RyanLin on 2025-05-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodMap/FoodMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMapApp.swift
+//  FoodMap
+//
+//  Created by RyanLin on 2025-05-21.
+//
+
+import SwiftUI
+
+@main
+struct FoodMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
